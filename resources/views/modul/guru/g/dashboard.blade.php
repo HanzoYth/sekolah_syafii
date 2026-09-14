3e51bd161c8a57bbdbc371a55d80a98b2d471156
HanzoYth <hanzoziu9@gmail.com>
2026-09-15 05:37:23 +0800

    s

--- resources/views/modul/guru/g/dashboard.blade.php
+++ resources/views/modul/guru/g/dashboard.blade.php
@@ -28,10 +28,6 @@
                     <p>Selamat datang kembali, <strong>{{$data_guru->nama}}</strong></p>
                 </div>
                 <div class="topbar-actions">
-                    <button class="icon-btn" title="Notifikasi">
-                        <i class="fa-regular fa-bell"></i>
-                        <span class="badge-dot"></span>
-                    </button>
                     <div class="user-profile">
                         <img src="{{ route('file.show',$data_guru->url_foto)}}" alt="Foto Profil">
                         <div class="user-info">
