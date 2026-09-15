3636de382bb0dfe670b4fb4eb251d29f7a5b304a
HanzoYth <hanzoziu9@gmail.com>
2026-09-15 10:31:52 +0800

    s:

--- database/migrations/2026_07_29_000014_create_pengumuman_table.php
+++ database/migrations/2026_07_29_000014_create_pengumuman_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
             $table->text("isi");
             $table->date("tanggal");
             $table->foreignId("guru_id")->constrained("guru")->cascadeOnDelete();
-            $table->foreignId("sekolah_id")->constrained("cabang_guru")->cascadeOnDelete();
+            $table->foreignId("sekolah_id")->constrained("jenis_sekolah")->cascadeOnDelete();
             $table->timestamps();
         });
     }
