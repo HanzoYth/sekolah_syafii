941de7170cb41a97087f86225aaf4f480f23a037
HanzoYth <hanzoziu9@gmail.com>
2026-09-15 13:51:41 +0800

    s

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -45,7 +45,7 @@
     </nav>
 
     <!-- ======== HERO SECTION ======== -->
-    <section id="home" class="hero" style="--hero-bg: url('{{ asset('public/img/ft_1.jpeg') }}');">
+    <section id="home" class="hero" style="--hero-bg: url('{{ asset('img/ft_1.jpeg') }}');">
         <div class="container">
             <div class="hero-wrapper">
                 <!-- Kiri: Teks -->
@@ -97,7 +97,7 @@
         <div class="container">
             <div class="keunggulan-wrapper">
                 <div class="keunggulan-img" data-aos="fade-right">
-                    <img src="{{asset('img/foto_1.jpeg')}}" alt="Mengaji">
+                    <img src="{{asset('public/img/ft_2.jpeg')}}" alt="Mengaji">
                 </div>
                 <div class="keunggulan-content" data-aos="fade-left">
                     <h2 class="section-title left">Mengapa Memilih Kami?</h2>
