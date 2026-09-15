58a2abc53d3f3213c0dd58ae99602c8b5fdd5602
HanzoYth <hanzoziu9@gmail.com>
2026-09-15 12:41:53 +0800

    s

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -45,7 +45,7 @@
     </nav>
 
     <!-- ======== HERO SECTION ======== -->
-    <section id="home" class="hero" style="--hero-bg: url('{{ public_path('img/ft_1.jpeg') }}');">
+    <section id="home" class="hero" style="--hero-bg: url('{{ asset('img/ft_1.jpeg') }}');">
         <div class="container">
             <div class="hero-wrapper">
                 <!-- Kiri: Teks -->
