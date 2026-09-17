6050cc39a17ce0b4d4fd55d89c52eabb76ecde7c
HanzoYth <hanzoziu9@gmail.com>
2026-09-17 10:44:12 +0800

    s

--- app/Http/Controllers/akunController.php
+++ app/Http/Controllers/akunController.php
@@ -237,7 +237,7 @@ class akunController extends Controller
 
         $data_akun->save();
 
-        return redirect("/reg");
+        return redirect("/reg")->with("success","Password anda sudah terganti");
 
 
     }
