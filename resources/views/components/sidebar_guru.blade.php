d616b21ef520c5c40e0d767956a6e6fa63b6afe2
HanzoYth <hanzoziu9@gmail.com>
2026-09-21 07:53:21 +0800

    us

--- resources/views/components/sidebar_guru.blade.php
+++ resources/views/components/sidebar_guru.blade.php
@@ -65,10 +65,10 @@
                             </a>
                         </li>
                         @if (App\Models\guru::where("id",session("id"))->first()->ast_krk)
-                            <li class="menu-item {{$route == '/gr/klgr ? 'active' : ''}}">
+                            <li class="menu-item {{$route == '/gr/klgr' ? 'active' : ''}}">
                                 <a href="/gr/edprgr">
                                     <i class="fa-solid fa-user"></i>
-                                    <span>Profile</span>
+                                    <span>Jadwal Piket</span>
                                 </a>
                             </li>
                         @endif
