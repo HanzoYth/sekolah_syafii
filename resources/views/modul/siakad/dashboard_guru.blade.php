03af369273e1c5d60e18712ab09a23824f83ff58
Enryu9145 <mohgiansaputra@gmail.com>
2026-09-22 08:28:02 +0800

    ubah url

--- resources/views/modul/siakad/dashboard_guru.blade.php
+++ resources/views/modul/siakad/dashboard_guru.blade.php
@@ -13,7 +13,7 @@
 
     <script src="https://cdn.tailwindcss.com"></script>
 
-    <link rel="stylesheet" href="{{ asset('css/modul/siakad/dashboard.css') }}?v={{ time() }}">
+    <link rel="stylesheet" href="{{ asset('css/modul/siakad/dasboard.css') }}?v={{ time() }}">
 </head>
 <body>
 
